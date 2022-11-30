a8c3eaed4ebfc186adb77d16095185bbf8c9ce48
marcoag1996 <115504846+marcoag1996@users.noreply.github.com>
2022-11-30 08:05:54 +0000

    PercolationStats finished

---
+++ Percolation/PercolationStats.java
@@ -0,0 +1,66 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+    private final WeightedQuickUnionUF normalQU;
+    private final double [] fractions;
+    private final double CONFIDENCE_95 = 1.96;
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n <= 0.");
+        }
+        if (trials <= 0) {
+            throw new IllegalArgumentException("trials <= 0.");
+        }
+        fractions = new double[trials];
+        Percolation percolation = new Percolation(n);
+        int countOpen = 0;
+
+        for(int i = 0; i < trials; i++) {
+            while(percolation.percolates() == false) {
+                int col = StdRandom.uniformInt(n) + 1; // base 1
+                int row = StdRandom.uniformInt(n) + 1; // base 1
+                if(!percolation.isOpen(row, col)) {
+                    percolation.open(row, col);
+                    countOpen++;
+                }
+            }
+            fractions[i] = countOpen / (n*n);
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(fractions);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(fractions);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return mean() - CONFIDENCE_95 * stddev() / Math.sqrt(fractions.length);
+    }
+
+    // high endpoint of 95% confidence interval 
+    public double confidenceHi() {
+        return mean() + CONFIDENCE_95 * stddev() / Math.sqrt(fractions.length);
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int t = Integer.parseInt(args[1]);
+
+        PercolationStats percolationStats = new PercolationStats(n, t);
+        StdOut.print("mean                    = " + percolationStats.mean());
+        StdOut.print("stddev                  = " + percolationStats.stddev());
+        StdOut.print("95% confidence interval = " + percolationStats.confidenceLo() + "," + percolationStats.confidenceHi());
+
+    }
+}
--- Percolation/percolation.java
+++ Percolation/percolation.java
@@ -1,7 +1,5 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
